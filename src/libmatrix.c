0247b1a7f3cad5aa5d2ca3b501aeb2036efa32b2
motecshine <shinemotec@gmail.com>
2017-08-20 16:52:15 +0800

    wip: ~

---
+++ src/libmatrix.c
@@ -0,0 +1,67 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "php.h"
+
+/* include global headers*/
+#include "php_matrix.h"
+#include "src/libmatrix.h"
+zend_class_entry * matrix_ce;
+ZEND_BEGIN_ARG_INFO(createMatrixArgInfo, 0)
+    ZEND_ARG_INFO(0, matrixData)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO(getMatrixRowArgInfo, 0)
+ZEND_ARG_INFO(0, rowNum)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO(getMatrixCol, 0)
+ZEND_ARG_INFO(0, colNum)
+ZEND_END_ARG_INFO()
+
+PHP_METHOD(Matrix, createMatrix)
+{
+    zval *matrixData;
+    ZEND_PARSE_PARAMETERS_START(1, 1)
+        Z_PARAM_ARRAY(matrixData)
+    ZEND_PARSE_PARAMETERS_END();
+    zend_update_property(matrix_ce, getThis(), ZEND_STRL("matrix"), matrixData);
+    RETURN_ZVAL(matrixData, 0, 0);
+}
+
+PHP_METHOD(Matrix, getMatrixRow)
+{
+    zend_long rowNum;
+    zval *matrixData, *entry;
+    zend_ulong h, hRow;
+    zend_string *key, *hKey;
+    ZEND_PARSE_PARAMETERS_START(1, 1)
+        Z_PARAM_LONG(rowNum)
+    ZEND_PARSE_PARAMETERS_END();
+
+    matrixData = zend_read_property(matrix_ce, getThis(), ZEND_STRL("matrix"), 0, matrixData);
+
+    ZEND_HASH_FOREACH_KEY_VAL(Z_ARRVAL_P(matrixData), h, key, entry) {
+       if (Z_TYPE_P(entry) == IS_ARRAY && rowNum == h) {
+            RETURN_ZVAL(entry, 0, 0);   
+       }
+    } ZEND_HASH_FOREACH_END();
+    RETURN_NULL();
+}
+
+
+static zend_function_entry matrix_methods[] = {
+    PHP_ME(Matrix, createMatrix, createMatrixArgInfo, ZEND_ACC_PUBLIC)
+    PHP_ME(Matrix, getMatrixRow, getMatrixRowArgInfo, ZEND_ACC_PUBLIC)
+    PHP_FE_END
+};
+
+PHP_MATRIX_STARTUP_FUNCTION(Matrix)
+{
+    zend_class_entry matrix_container_ce;
+    INIT_CLASS_ENTRY(matrix_container_ce, "Ml\\Matrix", matrix_methods);
+    matrix_ce = zend_register_internal_class(&matrix_container_ce TSRMLS_CC);
+    zend_declare_property_null(matrix_ce, ZEND_STRL("matrix"), ZEND_ACC_PUBLIC);
+    return SUCCESS;
+}
---
+++ src/libmatrix.h
@@ -0,0 +1,6 @@
+#ifndef LIBMATRIX_H
+#define LIBMATRIX_H 1
+PHP_METHOD(Matrix, createMatrix);
+PHP_MATRIX_STARTUP_FUNCTION(Matrix);
+#endif
+
